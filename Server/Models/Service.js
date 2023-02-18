3802d5d71032d53b667ae1d1e1321fba0c6bee24
Akshat Toolaj Sinha <2020csb1068@iitrpr.ac.in>
2023-02-18 22:43:58 +0530

    making models

--- Server/Models/Service.js
+++ Server/Models/Service.js
@@ -19,7 +19,7 @@ const ServiceSchema = new Schema ({
         maxlength: 100,
         required: [true, 'Description is required']
     },
-    servicedate: Date
+    serviceDate: Date
 })
 
 const Service = mongoose.model('Service', ServiceSchema)
