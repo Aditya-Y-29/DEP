093b4350bb7cbe4c816647145d57a00e95c59a55
Deepika <2020csb1084@iitrpr.ac.in>
2023-02-19 16:48:09 +0530

    calender

--- hello_world/lib/screens/add_expense.dart
+++ hello_world/lib/screens/add_expense.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../provider/data_provider.dart';
 import 'package:provider/provider.dart';
-
+import 'package:intl/intl.dart';
 
 class ExpenseScreen extends StatefulWidget {
   const ExpenseScreen({Key? key}) : super(key: key);
@@ -13,10 +13,17 @@ class ExpenseScreen extends StatefulWidget {
 class ExpenseData extends State<ExpenseScreen> {
 
   final _formKey = GlobalKey<FormState>();
+  TextEditingController dateController =TextEditingController();
 
   String communityDropDown='';
   String objectDropDown='';
 
+
+  @override
+  void initState(){
+    super.initState();
+    dateController.text="";
+  }
   @override
   Widget build(BuildContext context) {
 
@@ -76,6 +83,36 @@ class ExpenseData extends State<ExpenseScreen> {
               hintText: 'Amount',
             ),
             keyboardType: TextInputType.number,
+          ),
+          TextField(
+              controller: dateController, //editing controller of this TextField
+              decoration: const InputDecoration(
+                  icon: Icon(Icons.calendar_today), //icon of text field
+                  labelText: "Enter Date" //label text of field
+              ),
+              readOnly: true,  // when true user cannot edit text
+              onTap: () async {
+                //when click we have to show the datepicker
+                DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(), //get today's date
+                    firstDate:DateTime(1900), //DateTime.now() - not to allow to choose before today.
+                    lastDate: DateTime.now(),
+                );
+                if(pickedDate != null ){
+                 // print(pickedDate);  //get the picked date in the format => 2022-07-04 00:00:00.000
+                 String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate); // format date in required form here we use yyyy-MM-dd that means time is removed
+                 // print(formattedDate); //formatted date output using intl package =>  2022-07-04
+                  //You can format date as per your need
+
+                  setState(() {
+                    dateController.text = formattedDate.toString(); //set foratted date to TextField value.
+                  });
+                }else{
+                  //print("Date is not selected");
+                }
+              }
+
           ),
           TextFormField(
             decoration: const InputDecoration(
--- hello_world/lib/screens/add_service.dart
+++ hello_world/lib/screens/add_service.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../provider/data_provider.dart';
 import 'package:provider/provider.dart';
-
+import 'package:intl/intl.dart';
 
 
 
@@ -15,9 +15,16 @@ class ServiceScreen extends StatefulWidget {
 class ServiceData extends State<ServiceScreen> {
 
   final _formKey = GlobalKey<FormState>();
+  TextEditingController dateController =TextEditingController();
 
   String communityDropDown='';
   String objectDropDown='';
+
+  @override
+  void initState(){
+    super.initState();
+    dateController.text="";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -75,6 +82,36 @@ class ServiceData extends State<ServiceScreen> {
               icon: Icon(Icons.home),
               hintText: 'Service Name',
             ),
+          ),
+          TextField(
+              controller: dateController, //editing controller of this TextField
+              decoration: const InputDecoration(
+                  icon: Icon(Icons.calendar_today), //icon of text field
+                  labelText: "Enter Date" //label text of field
+              ),
+              readOnly: true,  // when true user cannot edit text
+              onTap: () async {
+                //when click we have to show the datepicker
+                DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(), //get today's date
+                    firstDate:DateTime(1900), //DateTime.now() - not to allow to choose before today.
+                    lastDate: DateTime.now(),
+                );
+                if(pickedDate != null ){
+                  // print(pickedDate);  //get the picked date in the format => 2022-07-04 00:00:00.000
+                   String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate); // format date in required form here we use yyyy-MM-dd that means time is removed
+                  // print(formattedDate); //formatted date output using intl package =>  2022-07-04
+                  //You can format date as per your need
+
+                  setState(() {
+                    dateController.text = formattedDate.toString(); //set foratted date to TextField value.
+                  });
+                }else{
+                  //print("Date is not selected");
+                }
+              }
+
           ),
           TextFormField(
             decoration: const InputDecoration(
