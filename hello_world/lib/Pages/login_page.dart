eebbe121fda6b7ce2fe6c45fedb963828e9a82d7
Pranavkumar Mallela <pranavmallela2020@gmail.com>
2023-03-10 21:59:38 +0530

    center login page

--- hello_world/lib/Pages/login_page.dart
+++ hello_world/lib/Pages/login_page.dart
@@ -1,182 +1,128 @@
 import 'package:flutter/material.dart';
-import 'signup_page.dart';
-import 'home_page.dart';
+import 'package:hello_world/Pages/home_page.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final _formKey = GlobalKey<FormState>();
-  late String _phoneNumber;
-  late String _password;
 
-  late int _counter = 0;
+  int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.all(10),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: 'Phone number',
-                    labelStyle: TextStyle(color: Colors.grey),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(
-                        color: Colors.blue,
-                        width: 2,
-                      ),
-                    ),
-                    filled: true,
-                    fillColor: Colors.white,
-                    prefixIcon: const Icon(
-                      Icons.phone,
-                      color: Colors.blue,
-                    ),),
-                keyboardType: TextInputType.phone,
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Please enter your phone number';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _phoneNumber = value!;
-                },
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container (
+              margin: const EdgeInsets.all(10),
+              child: const Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
               ),
-
-
-              if (_counter == 0)
-                Container(
-                  margin: EdgeInsets.all(5),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 225, 135, 18),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      minimumSize: Size(0, 50),
-                      padding: EdgeInsets.symmetric(vertical: 16),
-                      fixedSize: Size(80, 50),
-                    ),
-                    child: const Text(
-                      'Send OTP',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _counter++;
-                      });
-                    },
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
                   ),
+                  prefixIcon: Icon(Icons.phone),
+                  prefixIconColor: Colors.blue,
+                  hintText: 'Phone Number',
                 ),
-
-
-
-
-
-
-
-
-              if(_counter != 0)
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'OTP'),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value?.isEmpty ?? true) {
-                      return 'Please enter your OTP';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    _password = value!;
-                  },
+              ),
+            ),
+            if(_counter > 0)
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                  ),
+                  prefixIcon: Icon(Icons.lock),
+                  prefixIconColor: Colors.blue,
+                  hintText: 'OTP',
                 ),
-
-
-
-              if (_counter != 0)
-                Container(
-                  margin: EdgeInsets.all(5),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 225, 135, 18),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      minimumSize: Size(0, 50),
-                      padding: EdgeInsets.symmetric(vertical: 16),
-                      fixedSize: Size(120, 50),
-                    ),
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    onPressed: () {
-                      _counter = 0;
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const MyHomePage()),
-                      );
-                    },
-                  ),
-                ),
-
-
-
-              Container(
-                margin: EdgeInsets.all(5),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 225, 135, 18),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    minimumSize: Size(0, 50),
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                    fixedSize: Size(200, 50),
-                  ),
-                  child: Text(
-                    'Create an account',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  onPressed: () {
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(5),
+              child: ElevatedButton(
+                onPressed: () {
+                  if(_counter > 0) {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const SignupPage()),
+                      MaterialPageRoute(builder: (context) => const MyHomePage()),
                     );
-                  },
+                  }
+                  setState(() {
+                    _counter++;
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                  ),
+                ),
+                child:
+                _counter > 0 ? const Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                ) : const Text(
+                      'Send OTP',
+                      style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+              ),
+            ),
+            Container(
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                  ),
+                ),
+                child: const Text(
+                  'Create new account',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
                 ),
               ),
-            ],
-          ),
+            ),
+            Container(
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Forgot Password?',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
+    )
     );
   }
 }
