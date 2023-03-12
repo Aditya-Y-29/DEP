389690415823b018c8ba2640b0dd22f4e490e7ba
Pranavkumar Mallela <pranavmallela2020@gmail.com>
2023-03-12 16:04:36 +0530

    resolve expense working

--- hello_world/lib/provider/data_provider.dart
+++ hello_world/lib/provider/data_provider.dart
@@ -1,5 +1,6 @@
 
 import 'dart:developer';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:hello_world/components/expense.dart';
@@ -18,71 +19,72 @@ class DataProvider extends ChangeNotifier{
 
       Map<String, List<Expense>> objectUnresolvedExpenseMap = {
         "Oven": [
-          Expense(creator: "PersonA", description: "Oven Service", amount: 750, isPaid: false),
-          Expense(creator: "PersonB", description: "Oven Repair", amount: 1000, isPaid: false),
+          Expense(objectName: "Oven", creator: "PersonA", description: "Oven Service", amount: 750, isPaid: false),
+          Expense(objectName: "Oven", creator: "PersonB", description: "Oven Repair", amount: 1000, isPaid: false),
         ],
         "PC": [
-          Expense(creator: "PersonA", description: "PC Service", amount: 750, isPaid: false),
-          Expense(creator: "PersonB", description: "PC Repair", amount: 1000, isPaid: false),
+          Expense(objectName: "PC", creator: "PersonA", description: "PC Service", amount: 750, isPaid: false),
+          Expense(objectName: "PC", creator: "PersonB", description: "PC Repair", amount: 1000, isPaid: false),
         ],
         "TV": [
-          Expense(creator: "PersonA", description: "TV Service", amount: 750, isPaid: false),
-          Expense(creator: "PersonB", description: "TV Repair", amount: 1000, isPaid: false),
+          Expense(objectName: "TV", creator: "PersonA", description: "TV Service", amount: 750, isPaid: false),
+          Expense(objectName: "TV", creator: "PersonB", description: "TV Repair", amount: 1000, isPaid: false),
         ],
         "Fridge": [
-          Expense(creator: "PersonA", description: "Fridge Service", amount: 750, isPaid: false),
-          Expense(creator: "PersonB", description: "Fridge Repair", amount: 1000, isPaid: false),
+          Expense(objectName: "Fridge", creator: "PersonA", description: "Fridge Service", amount: 750, isPaid: false),
+          Expense(objectName: "Fridge", creator: "PersonB", description: "Fridge Repair", amount: 1000, isPaid: false),
         ],
         "Chairs": [
-          Expense(creator: "PersonA", description: "Chair Service", amount: 750, isPaid: false),
-          Expense(creator: "PersonB", description: "Chair Repair", amount: 1000, isPaid: false),
+          Expense(objectName: "Chairs", creator: "PersonA", description: "Chair Service", amount: 750, isPaid: false),
+          Expense(objectName: "Chairs", creator: "PersonB", description: "Chair Repair", amount: 1000, isPaid: false),
         ],
         "Books": [
-          Expense(creator: "PersonA", description: "Book Service", amount: 750, isPaid: false),
-          Expense(creator: "PersonB", description: "Book Repair", amount: 1000, isPaid: false),
+          Expense(objectName: "Books", creator: "PersonA", description: "Book Service", amount: 750, isPaid: false),
+          Expense(objectName: "Books", creator: "PersonB", description: "Book Repair", amount: 1000, isPaid: false),
         ],
         "Speaker": [
-          Expense(creator: "PersonA", description: "Speaker Service", amount: 750, isPaid: false),
-          Expense(creator: "PersonB", description: "Speaker Repair", amount: 1000, isPaid: false),
+          Expense(objectName: "Speaker", creator: "PersonA", description: "Speaker Service", amount: 750, isPaid: false),
+          Expense(objectName: "Speaker", creator: "PersonB", description: "Speaker Repair", amount: 1000, isPaid: false),
         ],
         "Car": [
-          Expense(creator: "PersonA", description: "Car Service", amount: 750, isPaid: false),
-          Expense(creator: "PersonB", description: "Car Repair", amount: 1000, isPaid: false),
+          Expense(objectName: "Car", creator: "PersonA", description: "Car Service", amount: 750, isPaid: false),
+          Expense(objectName: "Car", creator: "PersonB", description: "Car Repair", amount: 1000, isPaid: false),
         ],
         "Computer": [
-          Expense(creator: "PersonA", description: "Computer Service", amount: 750, isPaid: false),
-          Expense(creator: "PersonB", description: "Computer Repair", amount: 1000, isPaid: false),
+          Expense(objectName: "Computer", creator: "PersonA", description: "Computer Service", amount: 750, isPaid: false),
+          Expense(objectName: "Computer", creator: "PersonB", description: "Computer Repair", amount: 1000, isPaid: false),
         ],
       };
 
       Map<String, List<Expense>> objectResolvedExpenseMap = {
         "Oven": [
-          Expense(creator: "PersonA", description: "Oven Service", amount: 750, isPaid: true),
+          Expense(objectName: "Oven", creator: "PersonA", description: "Oven Service", amount: 750, isPaid: true),
         ],
         "PC": [
-          Expense(creator: "PersonA", description: "PC Service", amount: 750, isPaid: true),
+          Expense(objectName: "PC", creator: "PersonA", description: "PC Service", amount: 750, isPaid: true),
         ],
         "TV": [
-          Expense(creator: "PersonA", description: "TV Service", amount: 750, isPaid: true),
+          Expense(objectName: "TV", creator: "PersonA", description: "TV Service", amount: 750, isPaid: true),
         ],
         "Fridge": [
-          Expense(creator: "PersonA", description: "Fridge Service", amount: 750, isPaid: true),
+          Expense(objectName: "Fridge", creator: "PersonA", description: "Fridge Service", amount: 750, isPaid: true),
         ],
         "Chairs": [
-          Expense(creator: "PersonA", description: "Chair Service", amount: 750, isPaid: true),
+          Expense(objectName: "Chairs", creator: "PersonA", description: "Chair Service", amount: 750, isPaid: true),
         ],
         "Books": [
-          Expense(creator: "PersonA", description: "Book Service", amount: 750, isPaid: true),
+          Expense(objectName: "Books", creator: "PersonA", description: "Book Service", amount: 750, isPaid: true),
         ],
         "Speaker": [
-          Expense(creator: "PersonA", description: "Speaker Service", amount: 750, isPaid: true),
+          Expense(objectName: "Speaker", creator: "PersonA", description: "Speaker Service", amount: 750, isPaid: true),
         ],
         "Car": [
-          Expense(creator: "PersonA", description: "Car Service", amount: 750, isPaid: true),
+          Expense(objectName: "Car", creator: "PersonA", description: "Car Service", amount: 750, isPaid: true),
         ],
         "Computer": [
-          Expense(creator: "PersonA", description: "Computer Service", amount: 750, isPaid: true),
-          ],
+          Expense(objectName: "Computer", creator: "PersonA", description: "Computer Service", amount: 750, isPaid: true),
+        ],
+
       };
 
       Map<String, List<Service>> objectUnresolvedServices = {
@@ -173,7 +175,7 @@ class DataProvider extends ChangeNotifier{
 
       void addExpense(String objectName, String creator, int amount, String description)
       {
-        objectUnresolvedExpenseMap[objectName]?.add(Expense(creator: creator, amount: amount, description: description, isPaid: false));
+        objectUnresolvedExpenseMap[objectName]?.add(Expense(objectName: objectName, creator: creator, amount: amount, description: description, isPaid: false));
         // notifyListeners();
       }
 
@@ -181,6 +183,13 @@ class DataProvider extends ChangeNotifier{
       {
         objectUnresolvedServices[objectName]?.add(Service(creator: creator, description: description, isResolved: false));
         // notifyListeners();
+      }
+
+      void resolveExpense(Expense expense)
+      {
+        Expense? item = objectUnresolvedExpenseMap[expense.objectName]?.firstWhere((element) => element.objectName == expense.objectName && element.creator == expense.creator && element.amount == expense.amount && element.description == expense.description);
+        objectUnresolvedExpenseMap[expense.objectName]?.remove(item);
+        objectResolvedExpenseMap[expense.objectName]?.add(Expense(objectName: expense.objectName, creator: expense.creator, amount: expense.amount, description: expense.description, isPaid: true));
       }
 
 }
