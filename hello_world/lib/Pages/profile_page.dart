e9d473fa21db4b069e02c5bc2b07fef18f9218ab
Aditya-Y-29 <121538382+Aditya-Y-29@users.noreply.github.com>
2023-03-14 22:46:23 +0530

    Profile page added

--- hello_world/lib/Pages/profile_page.dart
+++ hello_world/lib/Pages/profile_page.dart
@@ -3,6 +3,13 @@ import 'package:hello_world/Pages/object_page.dart';
 import 'package:hello_world/Pages/community_page.dart';
 import 'package:hello_world/Pages/edit_profile_page.dart';
 
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:hello_world/constants.dart';
+import 'package:hello_world/widgets/profile_list_item.dart';
+
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
   @override
@@ -10,101 +17,179 @@ class ProfilePage extends StatefulWidget {
 }
 
 class ProfilePageState extends State<ProfilePage> {
-
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(50.0),
-
-          child:AppBar(
-            title: Text('My Profile'),
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.edit),
-          onPressed: () {
-           Navigator.push(
-             context,
-             MaterialPageRoute(builder: (context) => EditProfilePage()),
-           );
-          },
-        )
-      ],
-    ),
-          ),
-      body: ListView(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(left: 5.0, top: 5,right: 5.0),
-            padding:EdgeInsets.symmetric(horizontal: 2.0, vertical: 0.0),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.grey,
-                width: 2.0,
-              ),
-              borderRadius: BorderRadius.all(Radius.circular(8.0)),
-            ),
-            child: ListTile(
-              leading: Icon(Icons.person),
-              title: Text('Name'),
-              subtitle: Text('UserName'),//from database
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 5.0, top: 5,right: 5.0),
-            padding:EdgeInsets.symmetric(horizontal: 2.0, vertical: 0.0),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.grey,
-                width: 2.0,
-              ),
-              borderRadius: BorderRadius.all(Radius.circular(8.0)),
-            ),
-            child: ListTile(
-              leading: Icon(Icons.email),
-              title: Text('Email'),
-              subtitle: Text('user@email.com'),//from database
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 5.0, top: 5,right: 5.0),
-            padding:EdgeInsets.symmetric(horizontal: 2.0, vertical: 0.0),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.grey,
-                width: 2.0,
-              ),
-              borderRadius: BorderRadius.all(Radius.circular(8.0)),
-            ),
-            child: ListTile(
-              leading: Icon(Icons.phone_android),
-              title: Text('Mobile Number'),
-              subtitle: Text('123'),//from database
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 5.0, top: 5,right: 5.0),
-            padding:EdgeInsets.symmetric(horizontal: 2.0, vertical: 0.0),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.grey,
-                width: 2.0,
-              ),
-              borderRadius: BorderRadius.all(Radius.circular(8.0)),
-            ),
-            child: ListTile(
-              leading: Icon(Icons.details),
-              title: Text('Details'),
-              subtitle: Text('About'),//from database
-            ),
-          ),
-
-
-        ],
-      )
-
-        );
+    return ThemeProvider(
+      initTheme: kDarkTheme,
+      child: Builder(
+        builder: (context) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeProvider.of(context),
+            home: ProfileScreen(),
+          );
+        },
+      ),
+    );
   }
 }
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.init(context, height: 896, width: 414, allowFontScaling: true);
 
+    var profileInfo = Expanded(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: kSpacingUnit.w * 10,
+            width: kSpacingUnit.w * 10,
+            margin: EdgeInsets.only(top: kSpacingUnit.w * 3),
+            child: Stack(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: kSpacingUnit.w * 5,
+                  backgroundImage: AssetImage('assets/images/avatar.png'),
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Container(
+                    height: kSpacingUnit.w * 2.5,
+                    width: kSpacingUnit.w * 2.5,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).accentColor,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Center(
+                      heightFactor: kSpacingUnit.w * 1.5,
+                      widthFactor: kSpacingUnit.w * 1.5,
+                      child: Icon(
+                        LineAwesomeIcons.pen,
+                        color: kDarkPrimaryColor,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: kSpacingUnit.w * 2),
+          Text(
+            'Aditya Ladenberg',
+            style: kTitleTextStyle,
+          ),
+          SizedBox(height: kSpacingUnit.w * 0.5),
+          Text(
+            'Aditya@gmail.com',
+            style: kCaptionTextStyle,
+          ),
+          SizedBox(height: kSpacingUnit.w * 2),
+          // Container(
+          //   height: kSpacingUnit.w * 4,
+          //   width: kSpacingUnit.w * 20,
+          //   decoration: BoxDecoration(
+          //     borderRadius: BorderRadius.circular(kSpacingUnit.w * 3),
+          //     color: Theme.of(context).accentColor,
+          //   ),
+          //   child: Center(
+          //     child: Text(
+          //       'Upgrade to PRO',
+          //       style: kButtonTextStyle,
+          //     ),
+          //   ),
+          // ),
+        ],
+      ),
+    );
+
+    var themeSwitcher = ThemeSwitcher(
+      builder: (context) {
+        return AnimatedCrossFade(
+          duration: Duration(milliseconds: 200),
+          crossFadeState:
+          ThemeProvider.of(context).brightness == Brightness.dark
+              ? CrossFadeState.showFirst
+              : CrossFadeState.showSecond,
+          firstChild: GestureDetector(
+            onTap: () =>
+                ThemeSwitcher.of(context).changeTheme(theme: kLightTheme),
+            child: Icon(
+              LineAwesomeIcons.sun,
+              size: 20,
+            ),
+          ),
+          secondChild: GestureDetector(
+            onTap: () =>
+                ThemeSwitcher.of(context).changeTheme(theme: kDarkTheme),
+            child: Icon(
+              LineAwesomeIcons.moon,
+              size: 20,
+            ),
+          ),
+        );
+      },
+    );
+
+    var header = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(width: kSpacingUnit.w * 3),
+        Icon(
+          LineAwesomeIcons.arrow_left,
+          size: 20,
+        ),
+        profileInfo,
+        themeSwitcher,
+        SizedBox(width: kSpacingUnit.w * 3),
+      ],
+    );
+
+    return ThemeSwitchingArea(
+      child: Builder(
+        builder: (context) {
+          return Scaffold(
+            body: Column(
+              children: <Widget>[
+                SizedBox(height: kSpacingUnit.w * 5),
+                header,
+                Expanded(
+                  child: ListView(
+                    children: <Widget>[
+                      ProfileListItem(
+                        icon: LineAwesomeIcons.user_shield,
+                        text: 'Privacy',
+                      ),
+                      ProfileListItem(
+                        icon: LineAwesomeIcons.history,
+                        text: 'Purchase History',
+                      ),
+                      ProfileListItem(
+                        icon: LineAwesomeIcons.question_circle,
+                        text: 'Help & Support',
+                      ),
+                      ProfileListItem(
+                        icon: LineAwesomeIcons.cog,
+                        text: 'Settings',
+                      ),
+                      ProfileListItem(
+                        icon: LineAwesomeIcons.user_plus,
+                        text: 'Invite a Friend',
+                      ),
+                      ProfileListItem(
+                        icon: LineAwesomeIcons.alternate_sign_out,
+                        text: 'Logout',
+                        hasNavigation: false,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
