39a5bb173d6eff07d6f9d22a249a9f2beb761456
Aditya-Y-29 <121538382+Aditya-Y-29@users.noreply.github.com>
2023-03-18 10:58:11 +0530

    List of comm. added in navigation page

--- hello_world/lib/Pages/main_pages/navigation_page.dart
+++ hello_world/lib/Pages/main_pages/navigation_page.dart
@@ -32,23 +32,90 @@ class NavigationPageState extends State<NavigationPage> {
           return Column(
             // mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
+
+              // Container(
+              //   height: 50,
+              //   margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 15.0, bottom: 10.0),
+              //   padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 0, bottom: 8.0),
+              //   decoration: BoxDecoration(
+              //     borderRadius: BorderRadius.circular(10),
+              //     border: Border.all(
+              //       color: Colors.green,
+              //       width: 2,
+              //     ),
+              //     color: Colors.green.shade50,
+              //     boxShadow: const [
+              //       BoxShadow(
+              //         color: Colors.black26,
+              //         blurRadius: 10,
+              //         spreadRadius: 1,
+              //         offset: Offset(0, 2),
+              //       ),
+              //     ],
+              //   ),
+              //   child : GestureDetector(
+              //     onTap: () {
+              //       // Handle item click
+              //     },
+              //     child: ListTile(
+              //       title: Text('Archieved',
+              //         style: TextStyle(
+              //             fontSize: 21.0),
+              //       ),
+              //     ),
+              //   ),
+              // ),
+              // Container(
+              //   height: 50,
+              //   margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 15.0, bottom: 10.0),
+              //   padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 0, bottom: 8.0),
+              //   decoration: BoxDecoration(
+              //     borderRadius: BorderRadius.circular(10),
+              //     border: Border.all(
+              //       color: Colors.green,
+              //       width: 2,
+              //     ),
+              //     color: Colors.green.shade50,
+              //     boxShadow: const [
+              //       BoxShadow(
+              //         color: Colors.black26,
+              //         blurRadius: 2,
+              //         spreadRadius: 0,
+              //         offset: Offset(0, 1),
+              //       ),
+              //     ],
+              //   ),
+              //   child : GestureDetector(
+              //     onTap: () {
+              //       // Handle item click
+              //     },
+              //     child: ListTile(
+              //       title: Text('Reminders', style: TextStyle(
+              //           fontSize: 21.0),
+              //       ),
+              //     ),
+              //   ),
+              // ),
+              // Divider(
+              //     color: Colors.black
+              // ),
               Container(
                 height: 50,
-                margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 95.0, bottom: 10.0),
-                padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 0, bottom: 8.0),
+                margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 20.0, bottom: 10.0),
+                padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 0.0, bottom: 0.0),
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
+                  // borderRadius: BorderRadius.circular(10),
                   border: Border.all(
                     color: Colors.green,
-                    width: 2,
+                    width: 1,
                   ),
                   color: Colors.green.shade50,
                   boxShadow: const [
                     BoxShadow(
                       color: Colors.black26,
-                      blurRadius: 10,
-                      spreadRadius: 1,
-                      offset: Offset(0, 2),
+                      blurRadius: 2,
+                      spreadRadius: 0,
+                      offset: Offset(0, 1),
                     ),
                   ],
                 ),
@@ -57,45 +124,17 @@ class NavigationPageState extends State<NavigationPage> {
                     // Handle item click
                   },
                   child: ListTile(
-                    title: Text('Archieved',
+                    title: Text('Go To Communities >>> ',
                       style: TextStyle(
-                          fontSize: 21.0),
+                        fontSize: 18.0),
                     ),
                   ),
                 ),
               ),
-              Container(
-                height: 50,
-                margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 170.0, bottom: 10.0),
-                padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 0, bottom: 8.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(
-                    color: Colors.green,
-                    width: 2,
-                  ),
-                  color: Colors.green.shade50,
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.black26,
-                      blurRadius: 10,
-                      spreadRadius: 1,
-                      offset: Offset(0, 2),
-                    ),
-                  ],
-                ),
-                child : GestureDetector(
-                  onTap: () {
-                    // Handle item click
-                  },
-                  child: ListTile(
-                    title: Text('Reminders', style: TextStyle(
-                        fontSize: 21.0),
-                    ),
-                  ),
-                ),
+              Divider(
+                  color: Colors.black
               ),
-              ListView(
+              Wrap(
                   children:  List.of(communityDataProvider.communities.map((e) {
                     int k = communityDataProvider.communities.indexOf(e)+1;
                     return GestureDetector(
