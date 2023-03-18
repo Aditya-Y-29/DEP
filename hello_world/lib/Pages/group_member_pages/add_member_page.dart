d6b8cf027b30ddfecdbbdc239132416ee69b90ba
Pranavkumar Mallela <pranavmallela2020@gmail.com>
2023-03-18 12:57:34 +0530

    working on add_member page

--- hello_world/lib/Pages/group_member_pages/add_member_page.dart
+++ hello_world/lib/Pages/group_member_pages/add_member_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
 
 class AddMembers extends StatefulWidget {
   const AddMembers({Key? key}) : super(key: key);
@@ -8,11 +9,15 @@ class AddMembers extends StatefulWidget {
 }
 
 class _AddMemberState extends State<AddMembers> {
+  var selectedContacts = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add Members'),
+      ),
+      body: Container(
+
       ),
     );
   }
