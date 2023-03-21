b9e363141d6d414fa0904e3eaa09b7c007af1158
Pranavkumar Mallela <pranavmallela2020@gmail.com>
2023-03-21 12:27:40 +0530

    small changes

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:hello_world/Pages/group_member_pages/add_member_page.dart';
-import 'package:hello_world/Pages/group_member_pages/community_info_page.dart';
-
 import 'package:hello_world/Pages/main_pages/home_page.dart';
-import 'package:hello_world/Pages/profile_pages/profile_page.dart';
 import 'Pages/auth_pages/phone.dart';
-
 import 'provider/data_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -37,7 +31,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         home: isLoggedIn == null ? const MyPhone() : const MyHomePage(),
         routes: {
-        '/home': (context) => isLoggedIn == null ? const MyPhone() : const MyHomePage(),
+        '/home': (context) => const MyHomePage(),
       },
       ),
     );
