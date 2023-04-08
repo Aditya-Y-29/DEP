6663794d7b78732a353c67f0f6974f42e1e7b6e4
Aditya-Y-29 <121538382+Aditya-Y-29@users.noreply.github.com>
2023-04-09 01:54:30 +0530

    Improved UI of Info page

--- hello_world/lib/Pages/group_member_pages/community_info_page.dart
+++ hello_world/lib/Pages/group_member_pages/community_info_page.dart
@@ -36,44 +36,78 @@ class _CommunityInfoState extends State<CommunityInfo> {
       body: Container(
         child: Column(
           children: [
-            GestureDetector(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => AddMembers(communityName: widget.communityName)));
-              },
-              child: Container(
-                color: Colors.green.shade100,
-                padding: const EdgeInsets.all(13),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    CircleAvatar(
-                        radius: 20,
-                        child:  Icon(Icons.person_add),
-                    ),
-                    SizedBox(width: 10,),
-                    Text('Add Member', style: TextStyle(fontSize: 18),)
-                  ],
-                ),
-              )
-            ),
-            const SizedBox(height: 10,),
-            Container(
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10), bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10)),
-                border: Border.all(color: Colors.green, width: 2),
-              ),
-              child: const Text('Members', style: TextStyle(fontSize: 17),)
-            ),
+            // GestureDetector(
+            //   onTap: () {
+            //     Navigator.push(context, MaterialPageRoute(builder: (context) => AddMembers(communityName: widget.communityName)));
+            //   },
+            //   child: Container(
+            //     color: Colors.green.shade100,
+            //     padding: const EdgeInsets.all(13),
+            //     child: Row(
+            //       mainAxisAlignment: MainAxisAlignment.center,
+            //       children: const [
+            //         CircleAvatar(
+            //             radius: 20,
+            //             child:  Icon(Icons.person_add),
+            //         ),
+            //         SizedBox(width: 10,),
+            //         Text('Add Member', style: TextStyle(fontSize: 18),)
+            //       ],
+            //     ),
+            //   )
+            // ),
+            // const SizedBox(height: 10,),
+            // Container(
+            //   padding: const EdgeInsets.all(10),
+            //   decoration: BoxDecoration(
+            //     borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10), bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10)),
+            //     border: Border.all(color: Colors.green, width: 2),
+            //   ),
+            //   child: const Text('Members', style: TextStyle(fontSize: 17),)
+            // ),
             Expanded(
               child: 
                 ListView(
                   children: List.of(providerCommunity.communityMembersMap[widget.communityName] as Iterable<Widget>)
                 )
             )
+
           ],
         )
-      )
+      ),
+      bottomNavigationBar: BottomAppBar(
+      color: Colors.green.shade50,
+        elevation: 0,
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            SizedBox(width: 16.0,height: 10,),
+            Padding(
+              padding: const EdgeInsets.only(right: 85.0,bottom: 8),
+              child: FloatingActionButton(
+                onPressed:(){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => AddMembers(communityName: widget.communityName)));
+                },
+                child: Icon(Icons.person_add),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0,bottom: 8,top: 4),
+              child: FloatingActionButton(
+                onPressed: () async {
+                  DataProvider dataProvider =
+                  Provider.of<DataProvider>(context, listen: false);
+                  const snackbar1 = SnackBar(content: Text("Refreshing..."), duration: Duration(seconds: 8),);
+                  ScaffoldMessenger.of(context).showSnackBar(snackbar1);
+                  await dataProvider.getAllDetails(dataProvider.user!.phoneNo);
+                },
+                child: Icon(Icons.sync),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- hello_world/lib/components/member.dart
+++ hello_world/lib/components/member.dart
@@ -13,23 +13,36 @@ class _MemberState extends State<Member> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.green,
+          width: 2.0,
+        ),
+      ),
+      margin: const EdgeInsets.only(top: 5,right: 5,left: 5),
       padding: const EdgeInsets.all(10),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           CircleAvatar(
-            radius: 30,
+            radius: 20,
             child: Text(
-                widget.name[0],
-                style: const TextStyle(fontSize: 25),
+              widget.name[0],
+              style: const TextStyle(fontSize: 25),
             ),
           ),
-          const SizedBox(width: 10,),
           Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            // crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(widget.name, style: const TextStyle(fontSize: 20),),
-              Text(widget.phone, style: const TextStyle(fontSize: 15),),
-              widget.isCreator ? const Text('Creator', style: TextStyle(fontSize: 15, color: Colors.green),) : const Text('Member', style: TextStyle(fontSize: 15, color: Colors.red),)
+              Text(widget.name, style: const TextStyle(fontSize: 18),),
+              Text(widget.phone, style: const TextStyle(fontSize: 12),),
+            ],
+          ),
+          const SizedBox(width: 60,),
+          Column(
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              widget.isCreator ? const Text('Creator', style: TextStyle(fontSize: 15, color: Colors.blue),) : const Text('Member', style: TextStyle(fontSize: 15, color: Colors.grey),)
             ],
           )
         ],
