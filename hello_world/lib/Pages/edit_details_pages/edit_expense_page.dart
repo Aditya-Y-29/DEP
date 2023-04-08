041605827783ef74acee5803e5bc1e52343fea37
Aditya-Y-29 <121538382+Aditya-Y-29@users.noreply.github.com>
2023-04-09 00:57:11 +0530

    Edit expense changed

--- hello_world/lib/Pages/edit_details_pages/edit_expense_page.dart
+++ hello_world/lib/Pages/edit_details_pages/edit_expense_page.dart
@@ -3,6 +3,8 @@ import 'package:hello_world/screens/add_screens/resolve_expense_service.dart';
 import '../../screens/add_screens/edit_expense.dart';
 import '../../screens/add_screens/add_service.dart';
 import 'package:hello_world/components/expense.dart';
+import '../../provider/data_provider.dart';
+import 'package:provider/provider.dart';
 
 class EditExpensePage extends StatefulWidget {
 
@@ -18,19 +20,70 @@ class _EditFromObjectPageData extends State<EditExpensePage> {
 
   @override
   Widget build(BuildContext context) {
+    final providerCommunity = Provider.of<DataProvider>(context, listen: false);
     return DefaultTabController(
       length: 3,
 
       child: Scaffold(
         appBar: AppBar(
-          title: Text('${widget.expense.communityName}'),
-          bottom: const TabBar(
-            tabs: [
-              //Tab(icon: Icon(Icons.check_circle_outline),),
-              Tab(icon: Icon(Icons.currency_rupee_outlined),),
-              // Tab(icon: Icon(Icons.home_repair_service),),
+          elevation: 1,
+          title: Row(
+            children: <Widget>[
+              Image.asset(
+                '${providerCommunity.extractCommunityImagePathByName(widget.expense.communityName)}',
+                width: 40,
+                height: 40,
+              ),
+              SizedBox(width: 10),
+              Text(widget.expense.communityName),
             ],
-            indicatorColor: Colors.white,
+          ) ,
+          bottom: PreferredSize(
+              preferredSize: Size.fromHeight(90.0),
+              child: Container(
+                margin: EdgeInsets.only(left: 90),
+                child: Column(
+                    children:[
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 13.0,),
+                            child: Text('↳ ',
+                              style: TextStyle(
+                                fontSize: 30.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 10.0,top: 3),
+                            child: Image.asset(
+                              '${providerCommunity.extractObjectImagePathByName(widget.expense.objectName)}',
+                              width: 40,
+                              height: 40,
+                            ),
+                          ),
+
+                          Text(' ${widget.expense.objectName}',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                          children:[
+                            Padding(
+                              padding: EdgeInsets.only(bottom: 10.0,top: 3, left: 75),
+                              child: Icon(Icons.currency_rupee_outlined, color: Colors.white),
+                            ),
+                          ]
+                      )
+                    ]
+                ),
+              )
           ),
         ),
         body: TabBarView(
