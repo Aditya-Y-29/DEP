51680bb918380adc29c2d32cb67d31070b38706e
Pranavkumar Mallela <pranavmallela2020@gmail.com>
2023-04-21 15:03:25 +0530

    display creator

--- hello_world/lib/components/community.dart
+++ hello_world/lib/components/community.dart
@@ -107,20 +107,22 @@ class _CommunityState extends State<Community> {
                                   ),
                                   overflow: TextOverflow.ellipsis,
                                 ),
-                                // Row(
-                                //   mainAxisAlignment: MainAxisAlignment.center,
-                                //   children: [
-                                //     Icon(Icons.person_pin_circle_outlined, size: 14,),
-                                //     Text(
-                                //       providerCommunity.communityMembersMap[widget.creatorTuple]!.firstWhere((member) => member.phone == (widget.creatorTuple).split(":")[1], orElse: () => providerCommunity.communityMembersMap[widget.creatorTuple]!.firstWhere((member) => member.isCreator == true)).name,
-                                //       style: const TextStyle(
-                                //         color: Colors.black,
-                                //         fontSize: 10,
-                                //       ),
-                                //       overflow: TextOverflow.ellipsis,
-                                //     ),
-                                //   ],
-                                // )
+                                SizedBox(height: 5,),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.person_pin_circle_outlined, size: 14,),
+                                    Text(
+                                      providerCommunity.communityMembersMap.isEmpty ? "Creator" :
+                                      providerCommunity.communityMembersMap[widget.creatorTuple]!.firstWhere((member) => member.phone == (widget.creatorTuple).split(":")[1], orElse: () => providerCommunity.communityMembersMap[widget.creatorTuple]!.firstWhere((member) => member.isCreator == true)).name,
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 10,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
+                                )
                               ],
                               )
                         )
